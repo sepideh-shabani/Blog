0f55b38366334efd0e9a6b08c7c03e108e9ec17e
Sepideh Shabani <sepidehsh74@gmail.com>
2023-06-05 10:58:31 +0330

    create Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import Link from "next/link";
+
+function Navbar() {
+  const { username = null, user = null } = {};
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link href="/">
+            <button className="btn-blue">FEED</button>
+          </Link>
+        </li>
+        {username && (
+          <>
+            <li>
+              <Link href="/admin">
+                <button className="btn-blue">Write a Post</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${username}`}>
+                <Image src={user?.photoURL} alt="username" />
+              </Link>
+            </li>
+          </>
+        )}
+        {!username && (
+          <li>
+            <Link href="/enter">
+              <button className="btn-blue">Log In</button>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
